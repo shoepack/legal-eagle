1ee8d15b5a1fa47fbc6139e60f00f7a26011b654
Jason Artley <jtartley@yahoo.com>
2025-07-01 16:26:42 -0700

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,15 @@ export const metadata: Metadata = {
   title: "LegalUtils PDF Highlighter",
   description:
     "Automatically highlight Adjustments and Credit sections in legal invoices. Save time and improve accuracy with our intelligent PDF processing tool.",
+  icons: {
+    icon: [
+      { url: "/favicons/favicon.ico" },
+      { url: "/favicons/favicon-16x16.png", sizes: "16x16" },
+      { url: "/favicons/favicon-32x32.png", sizes: "32x32" },
+    ],
+    apple: "/favicons/apple-touch-icon.png",
+  },
+  manifest: "/favicons/site.webmanifest",
 };
 
 export default function RootLayout({
