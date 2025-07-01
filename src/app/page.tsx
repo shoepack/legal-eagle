92fc5a1535b35ab19ba041ce788e92cf3bb182e4
Jason Artley <jtartley@yahoo.com>
2025-06-30 18:38:59 -0700

    Fix overflow of page causing horizontal scroll bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
     };
   }, []);
   return (
-    <main className="min-h-screen relative">
+    <main className="min-h-screen relative overflow-x-hidden">
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
       {/* Hero Section */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-left">
